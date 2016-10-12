e9531a6683661c0c4c957d57838219d0df7c309a
vp <notejde@gmail.com>
2016-10-12 15:35:44 -0700

    add SASS

--- js/weather.js
+++ js/weather.js
@@ -3,11 +3,12 @@ var apiKey = require('./../.env').apiKey;
 Weather = function(){
 }
 
-Weather.prototype.getWeather = function(city) {
+Weather.prototype.getWeather = function(city, displayFunction) {
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-    return response.main.humidity;
+    displayFunction(city, response.main.humidity);
   }).fail(function(error) {
     $('.showWeather').text(error.responseJSON.message);
   });
+}
 
 exports.weatherModule = Weather;
